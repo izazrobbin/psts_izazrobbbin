a8d577d4034c51ed30abad5303fe126131a3d692
izaz.robbin <izaz.robbin2106@gmail.com>
2023-09-23 19:31:55 +0700

    first commit

---
+++ resources/views/customers/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>CRUD Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    
+
+<div class="container">
+    <h1>CRUD Customers Application</h1>
+    @yield('content')
+</div>
+
+    
+
+</body>
+
+</html>
